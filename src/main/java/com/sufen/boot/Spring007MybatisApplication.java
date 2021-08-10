271cf63508bbb8e2e31dd16a80d0acedd742e9c3
sufen <sufeng_shi@seu.edu.cn>
2021-08-10 16:26:43 +0800

    second commit~

--- src/main/java/com/sufen/boot/Spring007MybatisApplication.java
+++ src/main/java/com/sufen/boot/Spring007MybatisApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring007MybatisApplication {
 
     public static void main(String[] args) {
+        System.out.println("second commit~");
         SpringApplication.run(Spring007MybatisApplication.class, args);
     }
 
